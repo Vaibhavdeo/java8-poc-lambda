b8b36bd40c3af8e469d3e89a6cf4eeb9eec5d3a9
shivani_kabade <s.a.shivanand.kabade@accenture.com>
2018-07-20 11:54:48 +0530

    added one parm lambda impl

---
+++ java8_features/src/com/java/lambda/expression/LambdaExpWithOneParam.java
@@ -0,0 +1,55 @@
+package com.java.lambda.expression;
+
+@FunctionalInterface
+interface MyFunctionalInterface2 {
+
+	public String sayHello(String name);
+}
+
+@FunctionalInterface
+interface SingleParameterInterface {
+
+	public int squareOfFive(int s);
+}
+
+public class LambdaExpWithOneParam {
+
+	public static void main(String[] args) {
+
+		// 1st way
+		MyFunctionalInterface2 msg = (n) -> {
+
+			return "Hello" + n;
+		};
+		System.out.println(msg.sayHello("Smith"));
+
+		// 2nd way
+		MyFunctionalInterface2 msg2 = (n) -> "Hello" + n;
+		System.out.println(msg2.sayHello("VD"));
+
+		// 3rd way
+		MyFunctionalInterface2 msg3 = n -> "Hello" + n;
+		System.out.println(msg3.sayHello("DJ"));
+
+	
+		/*
+		 * SingleParameterInterface implementation
+		 */
+
+		// 1st way
+		SingleParameterInterface square = (no) -> {
+			return no * no;
+		};
+		System.out.println(square.squareOfFive(5));
+
+		// 2nd way
+		SingleParameterInterface square2 = (no) -> no * no;
+		System.out.println(square2.squareOfFive(5));
+
+		//3rd way
+		SingleParameterInterface square3 = no -> no * no;
+		System.out.println(square3.squareOfFive(5));
+
+	}
+
+}
