244a8e44119ebd7fa5922273a14bd441747f3db5
shivani_kabade <s.a.shivanand.kabade@accenture.com>
2018-07-20 15:55:14 +0530

    commit15

--- java8_features/src/com/java/example/method/reference/ConstructorMethodRefExample.java
+++ java8_features/src/com/java/example/method/reference/ConstructorMethodRefExample.java
@@ -1,25 +1,32 @@
 package com.java.example.method.reference;
 
-interface Messageble{
-	
+interface Messageble {
+
 	ConstructorMethodRefExample display(String msg);
-	
 }
 
 public class ConstructorMethodRefExample {
 	
-	ConstructorMethodRefExample(String msg){
+	public ConstructorMethodRefExample() {
+		System.out.println("Hellooo");
+	}
+
+	public ConstructorMethodRefExample(String msg) {
 		System.out.println(msg);
 	}
-	
+
 	public static void main(String[] args) {
-		
-		Messageble ref = (msg1)->new ConstructorMethodRefExample(msg1);
+
+		Messageble ref = (msg1) -> new ConstructorMethodRefExample(msg1);
 		ref.display("Hello World");
-		
+
 		Messageble ref2 = ConstructorMethodRefExample::new;
 		ref2.display("Hello Word2");
 		
-	}
+		//Signature shuuld be same
+		
+		/*Messageble ref3 = () -> new ConstructorMethodRefExample();
+		ref.display("Hello World");*/
 
+	}
 }
