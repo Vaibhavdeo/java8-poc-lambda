ae913ffde1f7bf9f6727bb22253b09a1cf395cd9
shivani_kabade <s.a.shivanand.kabade@accenture.com>
2018-07-20 14:49:18 +0530

    commit5

---
+++ java8_features/src/com/java/defaultstatic/methods/Example5.java
@@ -0,0 +1,33 @@
+package com.java.defaultstatic.methods;
+
+/*
+ * 
+ * Static method in interface
+ * 
+ */
+interface MyInterfaceTwo {
+
+	default int sumDefault(int a, int b) {
+		return a + b;
+	}
+
+	static int sumStatic(int a, int b) {
+		return a + b;
+	}
+
+}
+
+public class Example5 implements MyInterfaceTwo{
+	
+	public static void main(String[] args) {
+		
+		Example5 example5 = new Example5();
+		
+		System.out.println(example5.sumDefault(3, 4));
+		
+		System.out.println(MyInterfaceTwo.sumStatic(4, 5));
+		
+		
+	}
+
+}
